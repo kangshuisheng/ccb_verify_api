fc366ae003f3bff8cd07b261235fcaec6444e3f3
kangshuisheng <2538041184@qq.com>
2023-06-27 09:48:46 +0800

    fix:  add publickKey to CCBPaylaodDto

--- src/main/java/com/kongshum/verify/ccb_verify_payload/AppController.java
+++ src/main/java/com/kongshum/verify/ccb_verify_payload/AppController.java
@@ -18,11 +18,9 @@ public class AppController {
 			@RequestBody CCBPayloadDto payload) {
 		System.out.println(payload.sign);
 		System.out.println(payload.src);
-
+		System.err.println(payload.publicKey);
 		CCBSign.RSASig rsa = new RSASig();
-		rsa.setPublicKey(
-				"30819c300d06092a864886f70d010101050003818a003081860281807f7e06318918e4b40e0544af801b065e85b182866bdc1d2b2400c31c9ae64016ec349ba7682352b19372898c693895bbd84e71f03a32c59ee6e39011db3e80b567a413b4ecd76ebc0e82834606fdb0d595264bead63791b650d17cc9d5a4885dc913294d020bdc011e5c14f1d5d60d585528b3c29e1f6280394120a99ac6d979020111");
-
+		rsa.setPublicKey(payload.publicKey);
 		String sign = rsa.generateSigature(payload.src);
 		boolean res = rsa.verifySigature(sign, payload.src);
 		System.out.print(res);
@@ -34,4 +32,5 @@ public class AppController {
 class CCBPayloadDto {
 	String sign;
 	String src;
+	String publicKey;
 }
